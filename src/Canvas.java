d2854bcd29c8625c429513608081f194a3e63012
polarjarod <polarjarod@gmail.com>
2015-05-13 13:33:50 -0500

    Update Canvas.java

--- src/Canvas.java
+++ src/Canvas.java
@@ -58,88 +58,10 @@ public class Canvas extends JPanel implements Runnable {
   public void moveBall (int nx, int ny) {
     ballX = nx;
     ballY = ny;
-    this.width = this.getWidth();
-    this.length = this.getHeight();
     repaint();
   } // end moveBall
 
-  // Here we receive from the game container class the key pressed
-  public void keyPressed(KeyEvent e) {
-    switch(e.getKeyCode()) {
-      // Move ship 1
-      case KeyEvent.VK_W :
-        player1FlagArr = true;
-        break;
-      case KeyEvent.VK_S :
-        player1FlagAba = true;
-        break;
-      
-      // Move ship 2
-      case KeyEvent.VK_UP:
-        player2FlagArr=true;
-        break;
-      case KeyEvent.VK_DOWN:
-        player2FlagAba=true;
-        break;
-    } // end switch
-  } // end keyPressed
-
-  // Here we receive from the game container class the key released
-  public void keyReleased(KeyEvent e) {
-   switch(e.getKeyCode()) {
-    // Move player 1
-    case KeyEvent.VK_W :
-      player1FlagArr = false;
-      break;
-    case KeyEvent.VK_S :
-      player1FlagAba = false;
-      break;
-
-    // Move player 2
-    case KeyEvent.VK_UP:
-      player2FlagArr = false;
-      break;
-    case KeyEvent.VK_DOWN:
-      player2FlagAba = false;
-      break;
-    } // end switch
-  } // end keyReleased
-
-  // Move player 1
-  public void shouldMovePlayer1 () {
-    if (player1FlagArr == true && p1Y >= 0) {
-      p1Y += down;
-    }
-    if (player1FlagAba == true && p1Y <= (this.getHeight()-25)) {
-      p1Y += up;
-    }
-    movePlayer1(p1X, p1Y);
-    
-  } // end shouldMovePlayer1
-
-  // Move player 2
-  public void shouldMovePlayer2() {
-    if (player2FlagArr == true && p2Y >= 0) {
-      p2Y += down;
-    }
-    if (player2FlagAba == true && p2Y <= (this.getHeight()-25)) {
-      p2Y += up;
-    }
-    movePlayer2(p2X, p2Y);
-  }
-
-  // Position on Y for the player 1
-  public void movePlayer1(int x, int y){
-    this.p1X = x;
-    this.p1Y = y;
-    repaint();
-  }
-  // Position on Y for the player 2
-  public void movePlayer2(int x, int y){
-    this.p2X = x;
-    this.p2Y = y;
-    repaint();
-  }
+ 
 
   public void run() {
     // TODO Auto-generated method stub
@@ -174,13 +96,6 @@ public class Canvas extends JPanel implements Runnable {
             }
           }
           moveBall(ballX, ballY);
-          
-         // Delay
-          try {
-            Thread.sleep( 20 );
-          } catch(InterruptedException ex) {
-          
-          }
           // Move player 1
           shouldMovePlayer1();
           // Move player 2
