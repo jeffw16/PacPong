5bd7f5b7a23bba7965d9568c1163581e53f44849
Jeffrey Wang <jeffw16@users.noreply.github.com>
2015-05-14 10:38:17 -0500

    Update Canvas.java

--- src/Canvas.java
+++ src/Canvas.java
@@ -60,72 +60,15 @@ public class Canvas extends JPanel implements Runnable {
     ballY = ny;
     repaint();
   } // end moveBall
-
- 
-
+/*
   public void run() {
     // TODO Auto-generated method stub
     boolean ballright=false;
     boolean ballup=false;
     
     while ( true ) {
-      if ( game ) {
-        // The ball move from left to right
-        if ( ballright ) {
-          ballX += right;
-          if (ballX >= (width - 8))
-            ballright= false;
-          } else {
-            ballX += left;
-            if ( ballX <= 0) {
-              ballright = true;
-            }
-          }
-          // The ball moves from up to down
-          if ( ballup ) {
-            // go up
-            ballY += up;
-            if (ballY >= (length - 8)) {
-              ballup= false;
-            }
-          } else {
-            // go down
-            ballY += down;
-            if ( ballY <= 0 ) {
-              ballup = true;
-            }
-          }
-          moveBall(ballX, ballY);
-          // Move player 1
-          shouldMovePlayer1();
-          // Move player 2
-          shouldMovePlayer2();
-          // The score of the player 1 increase
-          if (ballX >= (width - 8)) {
-            score1++;
-          }
-          // The score of the player 2 increase
-          if ( ballX == 0 ) {
-            score2++;
-          }
 
-          // Game over. Here you can change 10 to any value
-          // When the score reach to the value, the game will end
-          if (score1 == 10 || score2 == 10 ) {
-            game = false;
-            gameOver = true;
-          }
-
-          // The ball stroke with the player 1
-          if ( ballX == p1X+10 && ballY >= p1Y && ballY <= ( p1Y+25 ) ) {
-            ballright=true;
-          }
-
-          // The ball stroke with the player 2
-          if(ballX==(p2X-5) && ballY>=p2Y && ballY<=(p2Y+25)) {
-            ballright=false;
-          }
-        }
-      }
-    }
+     }
+   }
+   */
 }
