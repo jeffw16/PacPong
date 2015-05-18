fe231473b4cf867ce4654eee8ebf47c402005fde
Jeffrey Wang <jeffw16@users.noreply.github.com>
2015-05-18 13:31:29 -0500

    Update PacPong.java

--- src/PacPong.java
+++ src/PacPong.java
@@ -2,30 +2,32 @@
  * PacPong
  * Runner
 */
-import java.util.*;
-import java.io.*;
+
+import java.util.Scanner;
 
 class PacPong {
   private int p1Y = 0;
   public static void main ( String[] args ) {
     Scanner chooseScreenSize = new Scanner ( System.in );
     System.out.println ( "Screen size? 1 = 800x600, 2 = 1600x900, 3 = fullscreen: " );
+    int screenSizeSelection;
     try {
-      int screenSizeSelection = chooseScreenSize.nextInt();
+      screenSizeSelection = chooseScreenSize.nextInt();
     } catch ( Exception e ) {
       System.out.println ( "Invalid input; we will default your selection to 800x600." );
-      int screenSizeSelection = 1;
+      screenSizeSelection = 1;
     }
+    int screenSizeX, screenSizeY;
     if ( screenSizeSelection == 1 ) {
-      int screenSizeX = 800;
-      int screenSizeY = 600;
+      screenSizeX = 800;
+      screenSizeY = 600;
     } else if ( screenSizeSelection == 2 ) {
-      int screenSizeX = 1600;
-      int screenSizeY = 900;
-    } else if ( screenSizeSelection == 3 ) {
-      int screenSizeX = java.awt.GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds().width;
-      int screenSizeY = java.awt.GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds().height;
-    } else {
+      screenSizeX = 1600;
+      screenSizeY = 900;
+    } else { // if ( screenSizeSelection == 3 ) {
+      screenSizeX = java.awt.GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds().width;
+      screenSizeY = java.awt.GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds().height;
+    //} else {
       // add failsafe later here
     }
     Listener l = new Listener();
@@ -34,7 +36,7 @@ class PacPong {
   public PacPong( Listener l, int screenSizeX, int screenSizeY ) {
     boolean gameRunning = true;
     Canvas canvas = new Canvas ( screenSizeX, screenSizeY );
-    canvas.paintcomponent;
+    canvas.paintComponent( ); // add object with type Graphics
     int X, Y;
     X = canvas.ballX();
     Y = canvas.ballY();
@@ -47,12 +49,12 @@ class PacPong {
       }
       // The ball move from left to right
       if ( ballright ) {
-        X += move;
-        if (X >= (width - 8)) {
+        X += move; // move not found
+        if (X >= (width - 8)) { // width not found
           ballright= false;
         }
       } else {
-        X += move;
+        X += move; // move not found
         if ( X <= 0) {
           ballright = true;
         }
@@ -60,13 +62,13 @@ class PacPong {
       // The ball moves from up to down
       if ( ballup ) {
         // go up
-        Y += move;
-        if (Y >= (length - 8)) {
+        Y += move; // move not found
+        if (Y >= (length - 8)) { // length not found
           ballup= false;
         }
       } else {
         // go down
-        Y += down;
+        Y += down; // down not found
         if ( Y <= 0 ) {
           ballup = true;
         }
@@ -91,6 +93,18 @@ class PacPong {
       } else if ( smp2 == -1 ) {
         canvas.movePlayer2D();
       }
+      
+      /**
+       * BELOW VARS ARE MISSING
+       * width
+       * score1
+       * score2
+       * game
+       * gameOver
+       * p1X
+       * p2X
+       * p2Y
+       */
       
       // The score of the player 1 increase
       if (X >= (width - 8)) {
