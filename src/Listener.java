424a41527b04d1f8746c4297b2c11fce84ab0a4b
polarjarod <polarjarod@gmail.com>
2015-05-19 19:46:18 -0500

    Update Listener.java

--- src/Listener.java
+++ src/Listener.java
@@ -1,79 +1,72 @@
+import java.awt.event.KeyEvent;
+
 /**
- * PacPong
- * Listener
+* PacPong
+* Listener
 */
-
 class Listener {
-
 private boolean player1Up, player1Down, player2Up, player2Down;
-
-  public Listener() {
-    
-  }
+public Listener() {
+}
 public void keyPressed(KeyEvent e) {
-   switch(e.getKeyCode()) {
-     // Move ship 1
-     case KeyEvent.VK_W :
-       player1Up = true;
-       break;
-     case KeyEvent.VK_S :
-       player1Down = true;
-       break;
-     
-     // Move ship 2
-     case KeyEvent.VK_UP:
-       player2Up=true;
-       break;
-     case KeyEvent.VK_DOWN:
-       player2Down=true;
-       break;
-   } // end switch
- } // end keyPressed
-
- // Here we receive from the game container class the key released
- public void keyReleased(KeyEvent e) {
-  switch(e.getKeyCode()) {
-   // Move player 1
-   case KeyEvent.VK_W :
-     player1Up = false;
-     break;
-   case KeyEvent.VK_S :
-     player1Down = false;
-     break;
-
-   // Move player 2
-   case KeyEvent.VK_UP:
-     player2Up = false;
-     break;
-   case KeyEvent.VK_DOWN:
-     player2Down = false;
-     break;
-   } // end switch
- } // end keyReleased
-
- // Move player 1
- public int shouldMovePlayer1 () {
-   if (player1Up == true && p1Y >= 0) {
-     return 1;
-   }
-   if (player1Down == true && p1Y <= (this.getHeight()-25)) {
-     return -1;
-   }
-   else{
-     return 0;
-   }
- } // end shouldMovePlayer1
-
- // Move player 2
- public int shouldMovePlayer2() {
-   if (player2Up == true && p2Y >= 0) {
-     return 1;
-   }
-   if (player2Down == true && p2Y <= (this.getHeight()-25)) {
-     return -1;
-   }
-   else{
-     return 0;
-   } 
- }
+switch(e.getKeyCode()) {
+// Move ship 1
+case KeyEvent.VK_W :
+player1Up = true;
+break;
+case KeyEvent.VK_S :
+player1Down = true;
+break;
+// Move ship 2
+case KeyEvent.VK_UP:
+player2Up=true;
+break;
+case KeyEvent.VK_DOWN:
+player2Down=true;
+break;
+} // end switch
+} // end keyPressed
+// Here we receive from the game container class the key released
+public void keyReleased(KeyEvent e) {
+switch(e.getKeyCode()) {
+// Move player 1
+case KeyEvent.VK_W :
+player1Up = false;
+break;
+case KeyEvent.VK_S :
+player1Down = false;
+break;
+// Move player 2
+case KeyEvent.VK_UP:
+player2Up = false;
+break;
+case KeyEvent.VK_DOWN:
+player2Down = false;
+break;
+} // end switch
+} // end keyReleased
+// Move player 1
+public int shouldMovePlayer1 (int p1Y, int height) {
+if (player1Up == true && p1Y >= 0) {
+return 1;
+}
+if (player1Down == true && p1Y <= (height-25)) {
+return -1;
+}
+else{
+return 0;
+}
+} // end shouldMovePlayer1
+// Move player 2
+public int shouldMovePlayer2(int p2Y, int height) {
+if (player2Up == true && p2Y >= 0) {
+return 1;
+}
+if (player2Down == true && p2Y <= (height-25)) {
+return -1;
+}
+else{
+return 0;
+}
+}
 }
