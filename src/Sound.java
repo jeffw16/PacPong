5d4104f0cbf10da84fc1499bc2b9db3446d84fb0
SovietBeans <nicholasroth13@gmail.com>
2015-05-21 18:19:24 -0500

    Update Sound.java

--- src/Sound.java
+++ src/Sound.java
@@ -3,22 +3,62 @@
  * Sound
 */
 
+
 import java.applet.Applet;
 import java.applet.AudioClip;
 import java.net.URL;
 
-public class AudioPlayer {
-	
-	 AudioClip sound;
-	
-		 void playSound(){
-			try {
-				URL url = AudioPlayer.class.getResource("/Sound/src/Windows Logon Sound.wav");
-			  	sound = Applet.newAudioClip(url);
-			  	sound.play();
-			} catch ( Exception e ) {
-				
-			}
-		}
+import java.awt.event.*;
+import java.awt.*;
+import java.applet.*;
+import java.net.URL;
 
-}
+
+public class Sound{
+	AudioClip paddlePing;
+	AudioClip death;
+
+	public Sound(){
+
+			System.out.println("Sound object created");		
+	
+		}
+	
+	void paddleHit(){
+		
+		URL paddleH = Sound.class.getResource("high ping.wav");
+		paddlePing = Applet.newAudioClip(paddleH);
+		paddlePing.play();
+		
+	}
+	
+	void wallHit(){
+		
+		URL wallH = Sound.class.getResource("low ping.wav");
+		paddlePing = Applet.newAudioClip(wallH);
+		paddlePing.play();
+		
+	}
+	
+	void scoreChange(){		
+		
+		URL scoreC = Sound.class.getResource("loser_beep.wav");
+		paddlePing = Applet.newAudioClip(scoreC);
+		paddlePing.play();
+		
+	}
+	
+	/**void playDeath()
+	{
+		try{click.stop();}catch(Exception e){System.out.println("Thou shall not stop Rally X music");}
+		URL urlDeath = music.class.getResource("explosion.wav");
+		death = Applet.newAudioClip(urlDeath);
+		death.loop();
+		System.out.println("DEATH TO ALL MISCREANTS");
+	}**/
+		
+	}
+
+
+
+	
