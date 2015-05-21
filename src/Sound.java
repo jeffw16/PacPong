384be8df23e604955d2bc28ae37956b0ffd6704e
polarjarod <polarjarod@gmail.com>
2015-05-21 18:32:29 -0500

    Update Sound.java

--- src/Sound.java
+++ src/Sound.java
@@ -15,50 +15,40 @@ import java.net.URL;
 
 
 public class Sound{
-	AudioClip paddlePing;
-	AudioClip death;
+	AudioClip paddlePing, wallPing, scorePing;
 
 	public Sound(){
 
-			System.out.println("Sound object created");		
+			System.out.println("Sound object created");
+			URL paddleH = Sound.class.getResource("high ping.wav");
+			paddlePing = Applet.newAudioClip(paddleH);
+			URL wallH = Sound.class.getResource("low ping.wav");
+			wallPing = Applet.newAudioClip(wallH);
+			URL scoreC = Sound.class.getResource("loser beep.wav");
+			scorePing = Applet.newAudioClip(scoreC);
 	
 		}
 	
 	void paddleHit(){
 		
-		URL paddleH = Sound.class.getResource("high ping.wav");
-		paddlePing = Applet.newAudioClip(paddleH);
+		
 		paddlePing.play();
 		
 	}
 	
 	void wallHit(){
 		
-		URL wallH = Sound.class.getResource("low ping.wav");
-		paddlePing = Applet.newAudioClip(wallH);
-		paddlePing.play();
+		
+		wallPing.play();
 		
 	}
 	
 	void scoreChange(){		
 		
-		URL scoreC = Sound.class.getResource("loser_beep.wav");
-		paddlePing = Applet.newAudioClip(scoreC);
-		paddlePing.play();
+		
+		scorePing.play();
 		
 	}
-	
-	/**void playDeath()
-	{
-		try{click.stop();}catch(Exception e){System.out.println("Thou shall not stop Rally X music");}
-		URL urlDeath = music.class.getResource("explosion.wav");
-		death = Applet.newAudioClip(urlDeath);
-		death.loop();
-		System.out.println("DEATH TO ALL MISCREANTS");
-	}**/
-		
 	}
 
 
-
-	
